666a185ead0d691bb7a66561c8d9733678686e1e
eremita0083 <schrodinger.s.cat0083@gmail.com>
2014-01-19 18:31:37 +0900

    2nccommit:connect3.0への対応

--- app.js
+++ app.js
@@ -1,42 +1,37 @@
-
 /**
  * Module dependencies.
  */
 
-var express = require('express')
-, http = require('http')
-, path = require('path')
-, io = require('socket.io');
+var express = require('express'), http = require('http'), path = require('path'), io = require('socket.io');
 
-var app = express()
-, server = require('http').createServer(app)
-, io = io.listen(server);
- 
+var app = express(), server = require('http').createServer(app), io = io.listen(server);
 
 // all environments
-app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
-});
- 
-app.configure('development', function(){
-  app.use(express.errorHandler());
+app.configure(function() {
+	app.set('port', process.env.PORT || 3000);
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'jade');
+	app.use(express.favicon());
+	app.use(express.logger('dev'));
+	// app.use(express.bodyParser()); connect3.0で使えなくなるよという警告が出るので、下の二行を追加し、この行をremoveする。
+	app.use(express.json());
+	app.use(express.urlencoded()); //express.bodyParserはこの2行に置き換えることができ、その場合multipart()はコネクト３で外されるよという警告はでなくなる
+	app.use(express.methodOverride());
+	app.use(app.router);
+	app.use(express.static(path.join(__dirname, 'public')));
 });
 
- 
+app.configure('development', function() {
+	app.use(express.errorHandler());
+});
+
 server.listen(app.get('port'))
 
- 
 io.sockets.on('connection', function(socket) {
-  socket.on('message:send', function(data) {
-    io.sockets.emit('message:receive', { message: data.message });
-  });
+	socket.on('message:send', function(data) {
+		io.sockets.emit('message:receive', {
+			message : data.message
+		});
+	});
 });
 
